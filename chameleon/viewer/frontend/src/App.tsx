27b013394ee2fc49d1256864b78d0a9cec908957
Kabil-Aymen <gjourno738@gmail.com>
2024-06-21 12:14:24 +0100

    added boilerplate for navigation bar

--- chameleon/viewer/frontend/src/App.tsx
+++ chameleon/viewer/frontend/src/App.tsx
@@ -8,24 +8,50 @@ import { Route, Routes } from "react-router-dom";
 
 import { GenerateMixedModal } from "./components/pages/GenerateMixedModal";
 
-import { BasicNavbar } from "./components/ri-components/navbars/BasicNavbar";
+import { BasicNavbar, NavContent } from "./components/ri-components/navbars/BasicNavbar";
 
 // JSON Viewer specific css
 import "react18-json-view/src/style.css";
 
 function App() {
+  const navContent: NavContent = {
+    title: "Chameleon",
+    description: "Model Input/Output Viewer",
+    githubLink: "https://github.com/facebookresearch/chameleon",
+    showHomeLink: true,
+    navItems: [
+      {
+        id: "item1",
+        url: "/item1",
+        title: "Item 1",
+        showArrowIcon: true,
+      },
+      {
+        id: "item2",
+        url: "/item2",
+        title: "Item 2",
+        showArrowIcon: false,
+      },
+      {
+        id: "item3",
+        title: "Item 3 (No URL)",
+        showArrowIcon: false,
+      },
+    ],
+  };
+
   return (
     <div>
       <BasicNavbar
-        content={{title: "Chameleon", description: "Model Input/Output Viewer"}}
+        content={navContent}
       />
 
       <Routes>
         <Route index path="*" element={
-            <div className="relative lg:px-12 px-5 flex flex-col">
-              <GenerateMixedModal />
-            </div>
-          }
+          <div className="relative lg:px-12 px-5 flex flex-col">
+            <GenerateMixedModal />
+          </div>
+        }
         />
       </Routes>
     </div>
